0693d4bd124f1c608b572287bf8579f6b7f79c3d
Igor Bragaia <igor.bragaia@gmail.com>
2020-10-25 13:54:08 -0300

    Add live code

--- lib/main.dart
+++ lib/main.dart
@@ -10,162 +10,164 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.pink,
+        primarySwatch: Colors.lightBlue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: MyHomePage(title: 'Pokedex'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<Map> pokemonsData;
+  List<Map> data = [];
 
-  Future<Map> fetchPokemonData(String url) async {
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  Future<Map> getUrlData(String url) async {
     final response = await http.get(url);
-
     if (response.statusCode == 200) {
       Map body = jsonDecode(response.body);
 
-      String abilities =
-          body["abilities"].map((el) => el["ability"]["name"]).join(" | ");
+      String name = body["name"];
+      String abilities = "";
+      for (int i = 0; i < body["abilities"].length; i++) {
+        String abilityName = body["abilities"][i]["ability"]["name"];
+
+        if (i > 0) abilities += " | ";
+
+        abilities += abilityName;
+      }
+
       return {
-        "name": body["name"],
+        "titulo": name,
+        "subtitulo": abilities,
         "isFav": false,
-        "abilities": abilities,
       };
     } else {
       return null;
     }
   }
 
-  Future<void> getPokemons() async {
-    List<Map> _pokemonsData = [];
+  Future<void> getData() async {
+    for (int i = 1; i <= 20; i++) {
+      String url = 'https://pokeapi.co/api/v2/pokemon/$i';
+      Map pokemon = await getUrlData(url);
 
-    String dataUrl, imgUrl;
-    for (int id = 1; id < 20; id++) {
-      dataUrl = 'https://pokeapi.co/api/v2/pokemon/$id';
-      imgUrl = 'https://pokeres.bastionbot.org/images/pokemon/$id.png';
+      if (pokemon != null) {
+        String imgUrl = 'https://pokeres.bastionbot.org/images/pokemon/$i.png';
+        pokemon["imageUrl"] = imgUrl;
 
-      Map pokemonData = await fetchPokemonData(dataUrl);
-      pokemonData["imgUrl"] = imgUrl;
-
-      _pokemonsData.add(pokemonData);
+        data.add(pokemon);
+      }
     }
-    setState(() {
-      pokemonsData = _pokemonsData;
-    });
+
+    setState(() {});
   }
 
-  void onPokemonFavTap(String name) {
-    for (int i = 0; i < pokemonsData.length; i++)
-      if (pokemonsData[i]['name'] == name)
+  void onTapListItem(String titulo) {
+    print(titulo);
+    for (int i = 0; i < data.length; i++)
+      if (data[i]["titulo"] == titulo)
         setState(() {
-          pokemonsData[i]['isFav'] = !pokemonsData[i]['isFav'];
+          data[i]["isFav"] = !data[i]["isFav"];
         });
   }
 
-  Widget buildPokemon(
-    String name,
-    String abilities,
-    String imgUrl,
+  Widget buildListItem(
+    String titulo,
+    String subtitulo,
     bool isFav,
+    String url,
   ) {
     return Container(
       margin: EdgeInsets.only(
-        left: 8,
-        right: 8,
-        top: 4,
-        bottom: 4,
+        left: 10,
+        right: 10,
+        top: 5,
+        bottom: 5,
       ),
-      padding: EdgeInsets.all(3),
       decoration: BoxDecoration(
         color: Colors.white,
-        borderRadius: BorderRadius.all(Radius.circular(4)),
+        borderRadius: BorderRadius.all(Radius.circular(10)),
       ),
       child: ListTile(
-        leading: Image.network(imgUrl),
+        onTap: () {
+          onTapListItem(titulo);
+        },
+        leading: Image.network(url),
+        trailing: Icon(
+          isFav ? Icons.star : Icons.star_border,
+          size: 40,
+          color: Colors.orangeAccent,
+        ),
         title: Text(
-          name,
+          titulo,
           style: TextStyle(
-            fontSize: 20,
+            fontSize: 32,
             fontWeight: FontWeight.bold,
           ),
         ),
         subtitle: Text(
-          abilities,
+          subtitulo,
           style: TextStyle(
             fontSize: 15,
             fontWeight: FontWeight.normal,
           ),
         ),
-        trailing: Icon(
-          isFav ? Icons.star : Icons.star_border,
-          color: Colors.orangeAccent,
-          size: 40,
-        ),
-        onTap: () => onPokemonFavTap(name),
       ),
     );
   }
 
-  Widget buildPokemons() {
-    List<Widget> pokemonsWidget = pokemonsData
-        .map((el) => buildPokemon(
-              el["name"],
-              el["abilities"],
-              el["imgUrl"],
-              el["isFav"],
-            ))
-        .toList();
+  Widget buildList() {
+    List<Widget> listItems = [];
+    for (int i = 0; i < data.length; i++) {
+      listItems.add(buildListItem(
+        data[i]["titulo"],
+        data[i]["subtitulo"],
+        data[i]["isFav"],
+        data[i]["imageUrl"],
+      ));
+    }
 
     return Container(
-      color: Colors.pink.withOpacity(0.2),
-      child: ListView(
-        children: pokemonsWidget,
-      ),
+      color: Colors.lightBlue.withOpacity(0.5),
+      child: ListView(children: listItems),
     );
   }
 
   Widget buildLoading() {
-    return Container(
-      color: Colors.pink.withOpacity(0.2),
-      child: Center(
-        child: Loading(
-          indicator: BallPulseIndicator(),
-          size: 100.0,
-          color: Colors.white,
-        ),
+    return Center(
+      child: Loading(
+        indicator: BallPulseIndicator(),
+        size: 80,
+        color: Colors.lightBlueAccent,
       ),
     );
-  }
-
-  @override
-  void initState() {
-    getPokemons();
-
-    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: pokemonsData == null ? buildLoading() : buildPokemons(),
-      ),
+      appBar: AppBar(title: Text(widget.title)),
+      body: data.length > 0 ? buildList() : buildLoading(),
     );
   }
 }
